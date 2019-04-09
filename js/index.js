c2e0d12213b25a7b7726cce37c9e4ed5209e1718
AlphaRomulus <rjordan1352@gmail.com>
2019-04-09 15:47:12 -0700

    Function Created to minimize efforts

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
 // Your code goes here
+
+
+function eventTypes(selector,eventType,cb){
+  let element = document.querySelectorAll(selector);
+  element.forEach(x => {
+    x.addEventListener(eventType,cb);
+  });
+}
+
+let dblClick = function (){
+  if(this.style.position !== 'fixed'){
+    this.style.position = 'fixed';
+    this.style.top = '25%';
+    this.style.left = '1rem';
+    this.style.right = '2rem';
+    this.style.width = "90%";
+    this.style.margin = 'auto';
+  }else{
+    this.removeAttribute("style");
+  }
+}
+
+let keyDown = function (){
+  let query = document.querySelector('h1.logo-heading').textContent;
+  let key = String(event.key);//query = query.toLowerCase;
+  query.includes(key) ? console.log(true):false;
+}
+
+let test = function(){
+  console.log(this);
+}
+
+eventTypes('div.destination > .btn','click',test);
+eventTypes('div img','dblclick',dblClick);
+eventTypes('body','keydown',keyDown);
+//eventTypes('window','resize',events);
+//eventTypes('window','load',events);
+//eventTypes('window','resize',events);
+//eventTypes('window','load',events);
+//eventTypes('window','resize',events);
+//eventTypes('window','load',events);
+//eventTypes('window','resize',events);
+
+
+
+
