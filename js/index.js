6f7a777fb17dc6b1b156bb55423ebfa175103740
AlphaRomulus <rjordan1352@gmail.com>
2019-04-10 11:15:15 -0700

    Got as far as I can, will continue working on this tonight

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // Your code goes here
-
-
+var tmp = {};//I Hold textContent Removed From Browser
 function eventTypes(selector,eventType,cb){
   let element = document.querySelectorAll(selector);
   element.forEach(x => {
@@ -13,7 +12,7 @@ let dblClick = function (){
     this.style.position = 'fixed';
     this.style.top = '25%';
     this.style.left = '1rem';
-    this.style.right = '2rem';
+    this.style.right = '1rem';
     this.style.width = "90%";
     this.style.margin = 'auto';
   }else{
@@ -28,13 +27,34 @@ let keyDown = function (){
 }
 
 let test = function(){
-  console.log(this);
+  console.log(this.parentNode.childNodes[3].textContent);
 }
 
-eventTypes('div.destination > .btn','click',test);
-eventTypes('div img','dblclick',dblClick);
-eventTypes('body','keydown',keyDown);
-//eventTypes('window','resize',events);
+let signUp = function(){
+  key = this.parentNode.childNodes[3];
+  height = this.parentNode.childNodes[3].offsetHeight;
+  form = createForm(height);
+
+  if(key.hasAttribute('style')){
+    key.className ="";
+    form.className = 'hidden';
+  } else{
+    this.parentNode.replaceChild(form,key);
+    key.className = "hidden";
+    this.textContent = 'Never Mind!';
+  }
+}
+
+let noDefault = function(){
+  event.preventDefault();
+  console.log(tmp)
+}
+
+eventTypes('div.destination > .btn','click',signUp);//Begin Client Conversion
+eventTypes('div img','dblclick',dblClick);//Pop Images
+eventTypes('body','keydown',keyDown);//Change Color of <h1> Char on Char down
+eventTypes('nav > a','click',noDefault);//Prevent Nav Link Defaults
+//eventTypes('window','resize',test);//FOR SURE DOES NOT WORK
 //eventTypes('window','load',events);
 //eventTypes('window','resize',events);
 //eventTypes('window','load',events);
@@ -45,3 +65,15 @@ eventTypes('body','keydown',keyDown);
 
 
 
+let createForm = function(height){
+  form = document.createElement('form');
+  formElementLabel = document.createElement('label');
+  formElementLabel.for = 'email';
+  formElement = document.createElement('input');
+  formElement.type = 'text';
+  formElement.name = 'email';
+  form.style.height = height + 'px';
+  form.appendChild(formElement);
+  return form;
+  
+}
